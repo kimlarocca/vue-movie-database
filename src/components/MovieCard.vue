01062b63eb06a74f73a26658e1fcd6487d406346
Kim LaRocca <kimlarocca@me.com>
2020-01-13 15:59:40 -0500

    updated code base

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="movie-card" @click="toggleDetails">
+        <img :src="'https://image.tmdb.org/t/p/w500'+image" :alt="title">
+        <h2>{{ title }}</h2>
+        <div class="details" v-if="visible">
+            <a class="button">X</a>
+            <p>{{ overview }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MovieCard',
+        props: {
+            title: String,
+            image: String,
+            visible: {
+                type: Boolean,
+                default: false
+            },
+            overview: String
+        },
+        methods: {
+            toggleDetails () {
+                this.visible = !this.visible
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+
+    .movie-card {
+        position: relative;
+        cursor: pointer;
+        opacity: 1;
+        transition: .2s all linear;
+        max-height: 281px;
+    }
+
+    .movie-card:hover {
+        opacity: .8;
+    }
+
+    h2 {
+        position: absolute;
+        bottom: 20px;
+        left: 20px;
+        color: white;
+        text-shadow: 1px 1px 0 #000;
+        margin: 0;
+    }
+
+    .details {
+        position: absolute;
+        width: calc(100% - 40px);
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        background: rgba(0, 0, 0, .85);
+        color: white;
+        padding: 30px 20px 20px;
+        z-index: 10;
+        text-align: left;
+        font-size: .875rem;
+    }
+
+    .button {
+        position: absolute;
+        top: 0;
+        right: 0;
+        color: white;
+        font-weight: bold;
+        border-radius: 0;
+        padding: 10px;
+    }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <div class="grid">
+            <div class="cell"
+                 v-for="(movie, index) in movies"
+                 :key="index">
+                <movie-card
+                        :title="movie.title"
+                        :image="movie.backdrop_path"
+                        :overview="movie.overview"
+                >
+                </movie-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import MovieCard from '../components/MovieCard'
+
+    export default {
+        name: 'HelloWorld',
+        components: {
+            'movie-card': MovieCard
+        },
+        data () {
+            return {
+                msg: 'Movie Database App',
+                movies: null
+            }
+        },
+        mounted () {
+            axios
+                .get('https://api.themoviedb.org/3/movie/popular?api_key=83d3415a37a96ef11c1ae22b98c556da&language=en-US&page=1')
+                .then(response => (this.movies = response.data.results))
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    a,
+    a:visited,
+    a:active {
+        cursor: pointer;
+        color: purple;
+    }
+
+    .grid {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+
+        .cell {
+            margin: 1rem;
+        }
+    }
+</style>
